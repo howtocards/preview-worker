5a440cabd662bc9f6959ae8798ac1efa94fa4d16
Sergey Sova <mail@sergeysova.com>
2019-08-29 21:59:13 +0300

    feat: implement it on puppeteer

---
+++ main.js
@@ -0,0 +1,25 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+const CARD = "http://localhost:3000/open/7";
+const cardPath = () =>
+  path.resolve(__dirname, `screenshots/${new Date().toISOString()}.png`);
+
+async function main() {
+  const browser = await puppeteer.launch();
+  try {
+    const page = await browser.newPage();
+    await page.goto(CARD, { waitUntil: "networkidle0" });
+    await page.setViewport({ deviceScaleFactor: 2, width: 1920, height: 1080 });
+
+    const el = await page.$("article");
+
+    await el.screenshot({
+      path: cardPath()
+    });
+  } finally {
+    await browser.close();
+  }
+}
+
+main().catch(console.error);
